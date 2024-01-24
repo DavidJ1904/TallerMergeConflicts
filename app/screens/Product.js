ada692b5ddd56f9b7d24baa46f267451305360b7
Edwin <Edwin_Alomoto@outlook.com>
2024-01-23 20:58:01 -0500

    commit listo

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -1,23 +1,43 @@
-import {View, Text, StyleSheet, Button} from 'react-native'
+import { View, Text, StyleSheet, Button } from "react-native";
 
-export const Product = ({navigation}) =>{
-    return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
-        <Button
-            title='HOME'
-            onPress={()=>{
-                navigation.navigate('HomeNav');
-            }}
-        />
+export const Product = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenido a mi tienda, soy Ariel Alomoto</Text>
+      <Text>Productos de ventas</Text>
+      <Text style={styles.producto1}>L/C NORMA </Text>
+      <Text style={styles.producto2}>ACAD NORMA </Text>
+      <Text style={styles.producto3}>CART BRIST A4 </Text>
+      <Button
+        title="HOME"
+        onPress={() => {
+          navigation.navigate("HomeNav");
+        }}
+      />
     </View>
-
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  producto1: {
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  producto2: {
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  producto3: {
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
